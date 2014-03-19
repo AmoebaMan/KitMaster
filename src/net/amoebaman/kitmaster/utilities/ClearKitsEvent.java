6f3f6ed49774f29f313b9bedaf42594b8b5460f9
Dennison Richter <drichter@amoebaman.net>
2014-03-19 12:32:36 -0400

    Add heroes to ClearKitsEvent

--- src/net/amoebaman/kitmaster/utilities/ClearKitsEvent.java
+++ src/net/amoebaman/kitmaster/utilities/ClearKitsEvent.java
@@ -17,62 +17,67 @@ public class ClearKitsEvent extends Event implements Cancellable {
 	
 	private static final HandlerList handlers = new HandlerList();
 	private Player player;
-	private boolean inventory, effects, permissions, cancelled;
+	private boolean inventory, effects, permissions, heroes, cancelled;
 	private ClearKitsContext context;
-
+	
 	/**
 	 * Do not construct this event.  All event firing/handling is done by KitMaster.
 	 */
-    public ClearKitsEvent(Player player, boolean inventory, boolean effects, boolean permissions, ClearKitsContext context){
+	public ClearKitsEvent(Player player, boolean inventory, boolean effects, boolean permissions, boolean heroes, ClearKitsContext context){
 		this.player = player;
 		this.inventory = inventory;
 		this.effects = effects;
 		this.permissions = permissions;
+		this.heroes = heroes;
 		this.context = context;
 		this.cancelled = false;
-    }
-    
-   	/**
-   	 * Do not construct this event.  All event firing/handling is done by KitMaster.
-   	 */
-       public ClearKitsEvent(Player player, ClearKitsContext context){
-   		this.player = player;
-   		this.inventory = true;
-   		this.effects = true;
-   		this.permissions = true;
-   		this.context = context;
-   		this.cancelled = false;
-       }
-    
-    /**
-     * Gets the player involved in this event.
-     * @return The player.
-     */
-    public Player getPlayer(){ return player; }
-  
-    public HandlerList getHandlers() { return handlers; }
-    public static HandlerList getHandlerList() { return handlers; }
-    
-    /**
-     * Calls this event, sending it through the Bukkit event system and giving other plugins the chance to modify it.
-     */
-    public void callEvent(){
-    	Bukkit.getServer().getPluginManager().callEvent(this);
-    }
+	}
+	
+	/**
+	 * Do not construct this event.  All event firing/handling is done by KitMaster.
+	 */
+	public ClearKitsEvent(Player player, ClearKitsContext context){
+		this.player = player;
+		this.inventory = true;
+		this.effects = true;
+		this.permissions = true;
+		this.heroes = true;
+		this.context = context;
+		this.cancelled = false;
+	}
+	
+	/**
+	 * Gets the player involved in this event.
+	 * @return The player.
+	 */
+	public Player getPlayer(){ return player; }
+	
+	public HandlerList getHandlers() { return handlers; }
+	public static HandlerList getHandlerList() { return handlers; }
+	
+	/**
+	 * Calls this event, sending it through the Bukkit event system and giving other plugins the chance to modify it.
+	 */
+	public void callEvent(){
+		Bukkit.getServer().getPluginManager().callEvent(this);
+	}
 	
 	public boolean isCancelled(){ return cancelled; }
-    public void setCancelled(boolean cancel){ this.cancelled = cancel; }
-
+	public void setCancelled(boolean cancel){ this.cancelled = cancel; }
+	
 	public boolean clearsInventory() { return inventory; }
 	public void setClearsInventory(boolean inventory) { this.inventory = inventory; }
-
+	
 	public boolean clearsEffects() { return effects; }
 	public void setClearsEffects(boolean effects) { this.effects = effects; }
-
+	
 	public boolean clearsPermissions() { return permissions; }
 	public void setClearsPermissions(boolean permissions) { this.permissions = permissions; }
 	
-	public boolean clearsAll(){ return inventory && effects && permissions; }
+	public boolean clearsHeroes(){ return heroes; }
+	public void setClearsHeroes(boolean heroes){ this.heroes = heroes; }
+	
+	public boolean clearsAll(){ return inventory && effects && permissions && heroes; }
 	public void setClearsAll(boolean value){
 		inventory = value;
 		effects = value;
